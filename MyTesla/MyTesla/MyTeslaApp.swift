bb1271e71bdfa8d995920ac8bfa32fa20910425a
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-05-14 00:04:21 +0500

    Подготовлен шаблон

---
+++ MyTesla/MyTesla/MyTeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTeslaApp.swift
+//  MyTesla
+//
+//  Created by Dmitriy Starozhilov on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyTeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
